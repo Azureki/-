c9b38e98ec0b9733cf2dfef031c4ca17be44e4a1
Azureki <fazureki@gmail.com>
2019-05-04 13:01:26 +0800

    more pythonic

--- 350. Intersection of Two Arrays II/main.py
+++ 350. Intersection of Two Arrays II/main.py
@@ -3,14 +3,9 @@ from collections import Counter
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
         c1 = Counter(nums1)
         c2 = Counter(nums2)
         # 随手写了一个dict的与运算，没想到真的可以。
         # 看来是取小值了
-        c = c1 & c2
-        res = []
-        for k, v in c.items():
-            res += [k] * v
-        return res
+        # c = c1 & c2
+        return list((c1&c2).elements())
